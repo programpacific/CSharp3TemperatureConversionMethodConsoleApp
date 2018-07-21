dc483279c2fbd8f2b8039076c8a0bc305e9bb23a
programpacific <programpacific@users.noreply.github.com>
2018-07-21 17:45:37 -0600

    Updating due to initial commit error.

---
+++ CSharpMethodsConsoleApp/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpMethodsConsoleApp
+{
+    public class CelciusConversion
+    {
+
+        public double Celcius(double fahTemp, double num2 = .556, double num3 = 32)
+        {
+            return (fahTemp - num3) * num2;
+        }
+    }
+
+    public class KelvinConversion
+    {
+        public double Kelvin(double fahTemp, double num2 = 273.15, double num3 = 32, double num4 = .556)
+        {
+            return (fahTemp - num3) * num4 + num2;
+        }
+    }
+
+    public class RankineConversion
+    {
+        public double Rankine(double fahTemp, double num2 = 273.15, double num3 = 9, double num4 = 5, double num5 = 32, double num6 = .556)
+        {
+            return (((fahTemp - num5) * num6) + num2) * (num3 / num4);
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- CSharpMethodsConsoleApp/Program.cs
+++ CSharpMethodsConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CSharpMethodsConsoleApp
 {
@@ -10,6 +7,48 @@ namespace CSharpMethodsConsoleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Welcome To The Temperature Conversion Tool");
+                Console.WriteLine();
+                Console.WriteLine("This tool will convert your *F value into");
+                Console.WriteLine("Celsius, Kelvin, and Rankine! ");
+                Console.WriteLine();
+                Console.WriteLine("What would you like to convert?");
+                double temperature = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                CelciusConversion sq = new CelciusConversion();
+                Console.WriteLine("Your temperature in Celsius is: " + sq.Celcius(temperature));
+
+                KelvinConversion su = new KelvinConversion();
+                Console.WriteLine("Your temperature in Kelvin is: " + su.Kelvin(temperature));
+
+                RankineConversion st = new RankineConversion();
+                Console.WriteLine("Your temperature in Rankine is: " + st.Rankine(temperature));
+                Console.WriteLine();
+
+                Console.WriteLine("Thank you for using my tool! Press enter to exit.");
+            }
+                        
+            catch (FormatException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("I'm sorry but that is not a valid number, please try again.");
+                Console.WriteLine("Press enter to exit");
+                return;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("I'm sorry but that is too large a number for me to calculate, please try again.");
+                Console.WriteLine("My operating parameters are -999999999 to 999999999");
+                Console.WriteLine("Press enter to exit");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
